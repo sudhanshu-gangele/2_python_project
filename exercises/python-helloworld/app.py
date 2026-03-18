b1d565c801d96075c50d45f2f1f0775bb9d9a263
sudhanshu-gangele <its.sudhanshu@gmail.com>
2026-03-18 02:31:37 -0400

    push changes

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 
 # Setup logging to a file
 formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
-file_handler = logging.FileHandler('app.log')
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
+# file_handler = logging.FileHandler('app.log')
+# file_handler.setLevel(logging.DEBUG)
+# file_handler.setFormatter(formatter)
 
 app.logger.setLevel(logging.DEBUG)
-app.logger.addHandler(file_handler)
+# app.logger.addHandler(file_handler)
 
 # Also capture werkzeug logs(for request info)
 werkzeug_logger = logging.getLogger('werkzeug')
 werkzeug_logger.setLevel(logging.INFO)
-werkzeug_logger.addHandler(file_handler)
+# werkzeug_logger.addHandler(file_handler)
 @app.route("/")
 def hello():
     app.logger.info("Main request successful")
@@ -41,5 +41,6 @@ def metrics():
     )
     app.logger.info("Metrics Successful")
     return response
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=6000)
